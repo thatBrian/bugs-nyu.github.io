64e65ca5f71b82edbac0063db9759ea237a9fbb0
thatBrian <yes229@nyu.edu>
2020-02-18 20:01:30 -0500

    Added Navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from "./components/home.component"
+import Navigation from "./components/navigation.component"
+import About from "./components/about.component"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation/>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route exact path="/about">
+          <About/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
