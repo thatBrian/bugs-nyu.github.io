203f5a83a64ad54c12acd825ccee511c13ee87e5
thatBrian <yes229@nyu.edu>
2020-02-18 20:02:14 -0500

    Home Componenet Complete

---
+++ client/src/components/home.component.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div id="home-container">
+      <div id="home-center">
+        <div id="home-welcome">
+          <img id="home-logo" src="./logo.svg"></img>
+          <div>
+          <span id="home-message">Welcome to </span>
+          <span id="bugs">BUGS</span>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
