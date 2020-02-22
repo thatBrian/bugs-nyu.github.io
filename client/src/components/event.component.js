042e4917e390eab81ac2d5aa6b64b8e18041184e
thatBrian <yes229@nyu.edu>
2020-02-21 22:45:21 -0500

    Added sample for event componenet, will be dynamically replaced

--- client/src/components/event.component.js
+++ client/src/components/event.component.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 function Event() {
     return (
-        <div id="events-container">
-            
+        <div className="event-container">
+            <div className="event-left">
+                <h2 className="event-title">
+                    5 Ways to animate a React app in 2019.
+                </h2>
+                <div>2020/2/21 <span>(Upcoming)</span></div>
+                <div className="event-grower">
+                    Animation in React app is a popular topic and there are many ways to create different types of animations.Many developers create…
+                </div>
+                <div className="event-footer" >
+                    Share stuff
+                </div>
+            </div>
+            <div className="event-right">
+                
+            </div>
         </div>
     );
 }
