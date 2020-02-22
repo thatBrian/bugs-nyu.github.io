0faaa557aa939d6ec8db0e0d85a3883d85ecb61d
thatBrian <yes229@nyu.edu>
2020-02-22 17:32:52 -0500

    About page component sample complete

--- client/src/components/about.component.js
+++ client/src/components/about.component.js
@@ -1,20 +1,54 @@
 import React from 'react';
-
+import Profile from "./profile.component"
 function About() {
     return (
         <div id="about-container">
+
             {/* <img src="./logo.svg"></img> */}
-            <div id="about-eboard">
-
+            <div className="about-block" id="about-eboard">
+                <div className="about-header">
+                    <h1>Current E-Board</h1>
+                </div>
+                <div className="profile-lister">
+                    <Profile />
+                    <Profile />
+                    <Profile />
+                    <Profile />
+                    <Profile />
+                </div>
             </div>
-            <div id="about-faculty">
-
+            <div className="about-block" id="about-faculty">
+                <div className="about-header">
+                    <h1>Faculty mentor</h1>
+                </div>
+                <div id="profile-lister-center">
+                    <Profile />
+                </div>
             </div>
-            <div id="about-alumni">
 
+            <div className="about-block" id="about-founders">
+                <div className="about-header">
+                    <h1>Founders</h1>
+                </div>
+                <div className="profile-lister">
+                    <Profile />
+                    <Profile />
+                    <Profile />
+                    <Profile />
+                    <Profile />
+                </div>
             </div>
-            <div id="about-founders">
-                
+            <div className="about-block" id="about-alumni">
+                <div className="about-header">
+                    <h1>Alumni</h1>
+                </div>
+                <div className="profile-lister">
+                    <Profile />
+                    <Profile />
+                    <Profile />
+                    <Profile />
+                    <Profile />
+                </div>
             </div>
         </div>
     );
