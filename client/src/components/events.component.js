619182664b5bc609617fbba3dc60b824da7f14ef
thatBrian <yes229@nyu.edu>
2020-02-21 22:46:12 -0500

    Completed events component

--- client/src/components/events.component.js
+++ client/src/components/events.component.js
@@ -1,9 +1,27 @@
 import React from 'react';
-
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Event from "./event.component"
 function Events() {
     return (
         <div id="events-container">
-            
+            <div id="events-header">
+                <Link to="/"><img className="home-logo" src="./logo.svg"></img></Link>
+                <h1>Events</h1>
+            </div>
+            <div id="events-content">
+                <Event></Event>
+                <Event></Event>
+                <Event></Event>
+                <Event></Event>
+                <Event></Event>
+                <Event></Event>
+            </div>
+
         </div>
     );
 }
