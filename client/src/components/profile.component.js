84f12ab09e9c719f943e0491c63c5353c3fd1b96
thatBrian <yes229@nyu.edu>
2020-02-22 14:18:27 -0500

    Profile component prop pass in complete

--- client/src/components/profile.component.js
+++ client/src/components/profile.component.js
@@ -1,23 +1,50 @@
 import React from 'react';
-
+import Dotdotdot from 'react-dotdotdot'
 function Profile(props) {
     return (
         <div className="profile-container">
             <div className="profile-left">
                 <div className="profile-image-container">
-                    <img></img>
+                    <img
+                        src={props.image && ("data:image/" + props.image.extension + ";base64, " + props.image.data)}
+                        alt={"Profile image of " + props.name}
+                    ></img>
+
                 </div>
+                {props.position && <span className="profile-position">President</span>}
             </div>
             <div className="profile-information">
                 <h3 className="profile-name">
-                    Name
-                    </h3>
+                    {props.name ? props.name : "No Name"}
+                </h3>
+
                 <div className="profile-description">
-                    Description: BlABLADJBLKJSDBLKSFJ
+                    {props.description ?
+                        <Dotdotdot clamp={5}>
+                            {props.description}
+                        </Dotdotdot> :
+                        "No Description"
+                    }
+                </div>
+                {props.link &&
+                    <div>
+                        {props.link.github &&
+                            <img className="social-icons github" src="./assets/github.svg"></img>
+                        }
+                        {props.link.facebook &&
+                            <img className="social-icons facebook" src="./assets/facebook.svg"></img>
+                        }
+                        {props.link.instagram &&
+                            <img className="social-icons instagram" src="./assets/instagram.svg"></img>
+                        }
+                        {props.link.linkedin &&
+                            <img className="social-icons linkedin" src="./assets/linkedin.svg"></img>
+                        }
+                        {props.link.website &&
+                            <img className="social-icons website" src="./assets/link.svg"></img>
+                        }
                     </div>
-                <div>
-                    Social Icons
-                    </div>
+                }
             </div>
         </div>
     );
