8493336da13ef40e67032ad835f6eae9dd06e8d1
thatBrian <yes229@nyu.edu>
2020-02-22 23:05:28 -0500

    Changed about -> team & made new about component

--- client/src/components/about.component.js
+++ client/src/components/about.component.js
@@ -1,82 +1,16 @@
 import React from 'react';
-import Profile from "./profile.component"
-function About(props) {
+
+function About() {
     return (
-        <div id="about-container">
-
-            {/* <img src="./logo.svg"></img> */}
-            <div className="about-block" id="about-eboard">
-                <div className="about-header">
-                    <h1>Current E-Board</h1>
-                </div>
-                <div className="profile-lister">
-                    {props.eboard.map((data, index) => {
-                        return <Profile
-                            key={data.name}
-                            image={data.image}
-                            position={data.position ? data.position : null}
-                            name={data.name}
-                            description={data.description}
-                            links={data.links}
-
-                        />
-                    })}
-                </div>
+        <div className="about-container">
+            <div>
+                <h3>Who are we?</h3>
             </div>
-            <div className="about-block" id="about-faculty">
-                <div className="about-header">
-                    <h1>Faculty mentor</h1>
-                </div>
-                <div id="profile-lister-center">
-                    {props.mentor.map((data, index) => {
-                        return <Profile
-                            key={data.name}
-                            image={data.image}
-                            position={data.position ? data.position : null}
-                            name={data.name}
-                            description={data.description}
-                            links={data.links}
-
-                        />
-                    })}
-                </div>
+            <div>
+                <h3>What is Open Source?</h3>
             </div>
-
-            <div className="about-block" id="about-founders">
-                <div className="about-header">
-                    <h1>Founders</h1>
-                </div>
-                <div className="profile-lister">
-                    {props.founders.map((data, index) => {
-                        return <Profile
-                            key={data.name}
-                            image={data.image}
-                            position={data.position ? data.position : null}
-                            name={data.name}
-                            description={data.description}
-                            links={data.links}
-
-                        />
-                    })}
-                </div>
-            </div>
-            <div className="about-block" id="about-alumni">
-                <div className="about-header">
-                    <h1>Alumni</h1>
-                </div>
-                <div className="profile-lister">
-                    {props.alum.map((data, index) => {
-                        return <Profile
-                            key={data.name}
-                            image={data.image}
-                            position={data.position ? data.position : null}
-                            name={data.name}
-                            description={data.description}
-                            links={data.links}
-
-                        />
-                    })}
-                </div>
+            <div>
+                <h3>Mozilla Partnership</h3>
             </div>
         </div>
     );
---
+++ client/src/components/team.component.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import Profile from "./profile.component"
+import {Link} from "react-router-dom";
+
+function Team(props) {
+    return (
+        <div id="about-container">
+
+            <Link to="/"><img className="home-logo" src="./logo_profile.svg"></img></Link>
+
+            <div className="about-block" id="about-eboard">
+                <div className="about-header">
+                    <h1>Current E-Board</h1>
+                </div>
+                <div className="profile-lister">
+                    {props.eboard.map((data, index) => {
+                        return <Profile
+                            key={data.name}
+                            image={data.image}
+                            position={data.position ? data.position : null}
+                            name={data.name}
+                            description={data.description}
+                            links={data.links}
+
+                        />
+                    })}
+                </div>
+            </div>
+            <div className="about-block" id="about-faculty">
+                <div className="about-header">
+                    <h1>Faculty mentor</h1>
+                </div>
+                <div id="profile-lister-center">
+                    {props.mentor.map((data, index) => {
+                        return <Profile
+                            key={data.name}
+                            image={data.image}
+                            position={data.position ? data.position : null}
+                            name={data.name}
+                            description={data.description}
+                            links={data.links}
+
+                        />
+                    })}
+                </div>
+            </div>
+
+            <div className="about-block" id="about-founders">
+                <div className="about-header">
+                    <h1>Founders</h1>
+                </div>
+                <div className="profile-lister">
+                    {props.founders.map((data, index) => {
+                        return <Profile
+                            key={data.name}
+                            image={data.image}
+                            position={data.position ? data.position : null}
+                            name={data.name}
+                            description={data.description}
+                            links={data.links}
+
+                        />
+                    })}
+                </div>
+            </div>
+            <div className="about-block" id="about-alumni">
+                <div className="about-header">
+                    <h1>Alumni</h1>
+                </div>
+                <div className="profile-lister">
+                    {props.alum.map((data, index) => {
+                        return <Profile
+                            key={data.name}
+                            image={data.image}
+                            position={data.position ? data.position : null}
+                            name={data.name}
+                            description={data.description}
+                            links={data.links}
+
+                        />
+                    })}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Team;
