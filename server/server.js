893cc89196a6325da0b388b9ebf5a3a9d4f40afa
thatBrian <yes229@nyu.edu>
2020-02-22 19:05:30 -0500

    Enabled CORs for development

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser= require('body-parser') // Handling Forms
 const app = express();
-var port = 3000;
+var port = 8000;
 var apiRouter = require('./routes/api');
-
+var cors = require('cors')
 // Middleware
+app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/api',apiRouter);
 
