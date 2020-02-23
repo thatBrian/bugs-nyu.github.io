d0110a3db949a8a3f9ebca9b17cccb994b020ba5
thatBrian <yes229@nyu.edu>
2020-02-22 23:06:26 -0500

    removed  unnecessary imports

--- client/src/components/events.component.js
+++ client/src/components/events.component.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import {Link} from "react-router-dom";
 import Event from "./event.component"
 function Events() {
     return (
         <div id="events-container">
             <div id="events-header">
-                <Link to="/"><img className="home-logo" src="./logo.svg"></img></Link>
+                <Link to="/"><img className="home-logo" src="./logo_events.svg"></img></Link>
                 <h1>Events</h1>
             </div>
             <div id="events-content">
